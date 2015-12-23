538c239d9263457c5d55e5788d6c4aa2ed928603
Jesse Cogollo <cogollo87@gmail.com>
2015-12-23 14:00:31 -0500

    third commit

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -1,9 +1,17 @@
 'use strict';
 
+var mongoHost = process.env.TDUSER_MONGO_HOST || 'localhost';
+
 // development specific configuration
 // =================================
 module.exports = {
   // Server port
-  port: process.env.PORT || 9101
+  port: process.env.PORT || 9001,
+  mongo: {
+    uri: 'mongodb://' + mongoHost + '/tduser-dev',
+    options: {
+      prefix: 'tduser_'
+    }
+  }
 
 };
