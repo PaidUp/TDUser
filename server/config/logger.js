e280dd9697809f0ee84dd5322293067bd729a3e3
Jesse Cogollo <cogollo87@gmail.com>
2016-02-09 12:52:18 -0500

    add loggly system

--- server/config/logger.js
+++ server/config/logger.js
@@ -1,35 +1,48 @@
 'use strict';
+
 var config = require('./environment');
-var fs = require( 'fs' );
-var logDir = config.root + '/var/logs'; // directory path you want to set
-if ( !fs.existsSync( logDir ) ) {
-    // Create the directory if it does not exist
-    fs.mkdirSync( config.root + '/var' );
-    fs.mkdirSync( config.root + '/var/logs' );
-}
 var winston = require('winston');
+require('winston-loggly');
+
+let env = process.env.NODE_ENV;
+
+var logger = new (winston.Logger)({
+    transports: [
+      new (winston.transports.Console)({
+        level: config.logger.level.info,
+        handleExceptions: true,
+        json: true,
+        colorize: true
+      }),
+      new (winston.transports.File)({
+        level: config.logger.level.warn,
+        handleExceptions: true,
+        json: true,
+        colorize: true,
+        filename: config.root + '/var/logs/all-logs.log',
+        maxsize: 5242880, //5MB
+        maxFiles: 5
+      }),
+      new (winston.transports.Loggly)({
+        level: config.logger.loggly.level,
+        handleExceptions: true,
+        json: true,
+        inputToken: config.logger.loggly.inputToken,
+        subdomain: config.logger.loggly.subdomain,
+        tags: config.logger.loggly.tags
+      })
+    ],
+    exitOnError: false
+  });
+
 winston.emitErrs = true;
 
-var logger = new winston.Logger({
-  transports: [
-    new winston.transports.File({
-      level: 'info',
-      filename: config.root + '/var/logs/all-logs.log',
-      handleExceptions: true,
-      json: true,
-      maxsize: 5242880, //5MB
-      maxFiles: 5,
-      colorize: false
-    }),
-    new winston.transports.Console({
-      level: 'debug',
-      handleExceptions: true,
-      json: false,
-      colorize: true
-    })
-  ],
-  exitOnError: false
-});
+if(env === 'test'){
+  logger
+    .remove(winston.transports.File)
+    .remove(winston.transports.Console)
+    .remove(winston.transports.Loggly);
+}
 
 module.exports = logger;
 module.exports.stream = {
