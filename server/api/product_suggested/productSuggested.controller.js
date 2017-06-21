028e93b7cec208df977470f89a0cdeb6db0a4f29
Ricardo Lara <riclara@gmail.com>
2017-06-20 22:34:35 -0400

    Update save product suggested

--- server/api/product_suggested/productSuggested.controller.js
+++ server/api/product_suggested/productSuggested.controller.js
@@ -3,28 +3,34 @@
 
 var Product = require('./ProductSuggested.model')
 
-function save (req, res) {
+function save(req, res) {
   var prod = new Product(req.body)
-  prod.save(function(err, data){
-    if(err){
+  prod.save(function (err, data) {
+    if (err) {
       return res.status(500).json({
         "message": err
       });
     }
-    res.status(200).json(data);
+    Product.find({ email: req.body.email }, function (err, products) {
+      if (err) {
+        return res.status(500).json({
+          "message": err
+        });
+      }
+      res.status(200).json({ products: products });
+    });
   })
 }
 
 function findByEmail(req, res) {
-  Product.find({email: req.params.email}, function (err, products) {
-    if(err){
+  Product.find({ email: req.params.email }, function (err, products) {
+    if (err) {
       return res.status(500).json({
         "message": err
       });
     }
-    res.status(200).json({products: products});
-  }
-  );
+    res.status(200).json({ products: products });
+  });
 }
 
 exports.save = save
