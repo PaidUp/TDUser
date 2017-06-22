5070c33e20a1039c0dd7d764bf05b0cec39593e2
Ricardo Lara <riclara@gmail.com>
2017-06-21 22:10:33 -0400

    Delete product

--- server/api/product_suggested/index.js
+++ server/api/product_suggested/index.js
@@ -8,6 +8,7 @@ var router = express.Router();
 
 //router.post('/products/save', authCoreService.isAuthenticatedServer(config.TDTokens.me), controller.save);
 router.post('/save',  controller.save);
+router.post('/delete',  controller.findAndDelete);
 router.get('/:email',  controller.findByEmail);
 
 module.exports = router;
--- server/api/product_suggested/productSuggested.controller.js
+++ server/api/product_suggested/productSuggested.controller.js
@@ -33,5 +33,26 @@ function findByEmail(req, res) {
   });
 }
 
+function findAndDelete(req, res) {
+  Product.find({ email: req.body.email, category: req.body.category  }, function (err, products) {
+    if (err) {
+      return res.status(500).json({
+        "message": err
+      });
+    }
+    if (products.length === 1){
+      products[0].remove();
+    } else {
+      products.forEach(function(prod) {
+        if(prod.product === req.body.product){
+          prod.remove()
+        }
+      });
+    }
+    res.status(200).json({ result: true });
+  });
+}
+
 exports.save = save
 exports.findByEmail = findByEmail
+exports.findAndDelete = findAndDelete
